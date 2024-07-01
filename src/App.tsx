9353b3cd13a00b38037f28277d579b441720fc02
Amir Khosravi <amirkhosravi921@gmail.com>
2024-07-01 16:26:54 +0330

    Debug: Change import issue

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ import { Genre } from "./hooks/useGenres";
 import PlatformSelector from "./components/PlatformSelector";
 import { Platform } from "./hooks/useGames";
 import SortSelector from "./components/SortSelector";
-import GameHeading from "./components/Gameheading";
+import GameHeading from "./components/GameHeading";
 
 export interface GameQuery {
   genre: Genre | null;
